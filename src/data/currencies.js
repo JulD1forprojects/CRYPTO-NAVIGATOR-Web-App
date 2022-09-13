29fbb9527163607a4fc7e38e36db91487ec2f198
Jul-JY <julietta198307@gmail.com>
2022-09-13 21:51:51 +0800

    creating an new file with fiat curreccies

---
+++ src/data/currencies.js
@@ -0,0 +1,180 @@
+const currencies = [
+  {
+    name: "Argentine Peso",
+    value: "ARS",
+  },
+  {
+    name: "Australian Dollar",
+    value: "AUD",
+  },
+  {
+    name: "Bahraini Dinar",
+    value: "BHD",
+  },
+  {
+    name: "Bangladeshi Taka",
+    value: "BDT",
+  },
+  {
+    name: "Bermudan Dollar",
+    value: "BMD",
+  },
+  {
+    name: "Brazilian Real",
+    value: "BRL",
+  },
+  {
+    name: "British Pound Sterling",
+    value: "GBP",
+  },
+  {
+    name: "Canadian Dollar",
+    value: "CAD",
+  },
+  {
+    name: "Chilean Peso",
+    value: "CLP",
+  },
+  {
+    name: "Chinese Yuan",
+    value: "CNY",
+  },
+  {
+    name: "Euro",
+    value: "EUR",
+  },
+  {
+    name: "Hong Kong Dollar",
+    value: "HKD",
+  },
+  {
+    name: "Hungarian Forint",
+    value: "HUF",
+  },
+  {
+    name: "Indian Rupee",
+    value: "INR",
+  },
+  {
+    name: "Indonesian Rupiah",
+    value: "IDR",
+  },
+  {
+    name: "Israeli New Sheqel",
+    value: "ILS",
+  },
+  {
+    name: "Japanese Yen",
+    value: "JPY",
+  },
+  {
+    name: "Kuwaiti Dinar",
+    value: "KWD",
+  },
+  {
+    name: "Malaysian Ringgit",
+    value: "MYR",
+  },
+  {
+    name: "Mexican Peso",
+    value: "MXN",
+  },
+  {
+    name: "Myanmar Kyat",
+    value: "MMK",
+  },
+  {
+    name: "New Taiwan Dolla",
+    value: "TWD",
+  },
+  {
+    name: "New Zealand Dollar",
+    value: "NZD",
+  },
+  {
+    name: "Nigerian Naira",
+    value: "NGN",
+  },
+  {
+    name: "Norwegian Krone",
+    value: "NOK",
+  },
+  {
+    name: "Pakistani Rupee",
+    value: "PKR",
+  },
+  {
+    name: "Philippine Peso",
+    value: "PHP",
+  },
+  {
+    name: "Polish Zloty",
+    value: "PLN",
+  },
+  {
+    name: "Russian Ruble",
+    value: "RUB",
+  },
+  {
+    name: "Saudi Riyal",
+    value: "SAR",
+  },
+  {
+    name: "Singapore Dollar",
+    value: "SGD",
+  },
+  {
+    name: "South African Rand",
+    value: "ZAR",
+  },
+  {
+    name: "South Korean Won",
+    value: "KRW",
+  },
+  {
+    name: "Special Drawing Rights",
+    value: "XDR",
+  },
+  {
+    name: "Sri Lankan Rupee",
+    value: "LKR",
+  },
+  {
+    name: "Swedish Krona",
+    value: "SEK",
+  },
+  {
+    name: "Swiss Franc",
+    value: "CHF",
+  },
+  {
+    name: "Thai Baht",
+    value: "THB",
+  },
+  {
+    name: "Turkish Lira",
+    value: "TRY",
+  },
+  {
+    name: "Ukrainian Hryvnia",
+    value: "UAH",
+  },
+  {
+    name: "United Arab Emirates Dirham",
+    value: "AED",
+  },
+  {
+    name: "US Dollar",
+    value: "USD",
+  },
+  {
+    name: "Venezuelan BolÃ­var",
+    value: "VEF",
+  },
+  {
+    name: "Vietnamese Dong",
+    value: "VND",
+  },
+];
+
+export default currencies;
